a4dd411186b7b3d02d7d1bcc3f65656d3e8c1c43
chenshijie <chrischen369@outlook.com>
2018-12-18 10:02:48 +0800

    movie

--- Refactoring/src/Rental.java
+++ Refactoring/src/Rental.java
@@ -1,14 +1,20 @@
 import java.util.Date;
 
-class Rental extends Movie {
-
+class Rental {
+    private Movie _movie;
     private int _daysRented;
-    public Rental(String title, int priceCode, Date start, Date end) {
-        super(title, priceCode);
+    public Rental(Movie movie, Date start, Date end) {
+        _movie = movie;
         _daysRented = (int)((end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24));
     }
     public int getDaysRented() {
         return _daysRented;
+    }
+    public int getPriceCode(){
+        return _movie.getPriceCode();
+    }
+    public String getTitle(){
+        return _movie.getTitle();
     }
 
 }
