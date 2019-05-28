24b726d292cc5169257f362de791e616a348e5ca
Chris <Chrischen369@outlook.com>
2019-05-28 20:38:48 +0800

    finish proj2

---
+++ CS302_OS/CS302-OS-Project2/pintos/src/userprog/process.h
@@ -0,0 +1,12 @@
+#ifndef USERPROG_PROCESS_H
+#define USERPROG_PROCESS_H
+
+#include "threads/thread.h"
+#define MAX_ARG_NUM 30
+#define MAX_ARG_LEN 100
+tid_t process_execute (const char *file_name);
+int process_wait (tid_t);
+void process_exit (void);
+void process_activate (void);
+
+#endif /* userprog/process.h */
---
+++ CS302_OS/CS302-OS-Project2/pintos/src/userprog/syscall.h
@@ -0,0 +1,6 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+void syscall_init (void);
+void bad_exit();
+#endif /* userprog/syscall.h */
