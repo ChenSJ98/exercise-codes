d06f6d669a69196bcd8be21d4f167da10ec046a5
RangerX-36917 <chrischen369@outlook.com>
2019-05-30 23:40:51 +0800

    finish create user by admin

---
+++ CS316_Cloud_Computing/AdminSys/src/test/java/com/sustech/AdminSys/AdminSysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sustech.AdminSys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminSysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
